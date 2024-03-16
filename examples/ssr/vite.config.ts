dc979020313967ffed71f56b8cda224e64a2ca5f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-16 16:06:19 +0900

    chore: reorganize examples  (#193)

---
+++ examples/ssr/vite.config.ts
@@ -0,0 +1,29 @@
+import process from "node:process";
+import globRoutesPlugin from "@hiogawa/vite-glob-routes";
+import { importDevServerPlugin } from "@hiogawa/vite-import-dev-server";
+import { vitePluginSsrMiddleware } from "@hiogawa/vite-plugin-ssr-middleware";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig((ctx) => ({
+  plugins: [
+    react() as any,
+    globRoutesPlugin({ root: "/src/routes" }),
+    importDevServerPlugin(),
+    vitePluginSsrMiddleware({
+      entry: process.env["SERVER_ENTRY"] ?? "./src/server/adapter-node.ts",
+    }),
+  ],
+  build: {
+    outDir: ctx.isSsrBuild ? "dist/server" : "dist/client",
+    manifest: ".vite/manifest.json",
+    sourcemap: true,
+  },
+  server: process.env["PORT"]
+    ? {
+        port: Number(process.env["PORT"]),
+        strictPort: true,
+      }
+    : undefined,
+  clearScreen: false,
+}));
